85996898be6c0260ccb024f37eae92678c8f11a0
pydai <7242067+pydai@users.noreply.github.com>
2019-10-13 10:06:40 -0700

    Update MaxSubarray.js

--- MaxSubarray.js
+++ MaxSubarray.js
@@ -1,3 +1,4 @@
+// Sum all the previous values until the currentSum is negative. If so, reset the sum to 0. Track the maxSum as you go.
 var maxSubArray = function(nums) {
     let sum = 0;
     let maxSum = 0;
@@ -13,3 +14,17 @@ var maxSubArray = function(nums) {
     })
     return maxSum;
 };
+
+// Keep a count of the current sum by either the current value in the array or the current sum plus the current value.
+// Update the max sum as you go.
+var maxSubArray = function(nums) {
+    let currentSum = nums[0];
+    let maxSum = nums[0];
+    
+    for(let i = 1; i < nums.length; i++) {
+        let current = nums[i];
+        currentSum = Math.max(current, current + currentSum);
+        maxSum = Math.max(currentSum, maxSum);
+    }
+    return maxSum;
+}
