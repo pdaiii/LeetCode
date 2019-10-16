e88b8c2751535f5df8ac850a9a7a5851e3c7d345
pydai <7242067+pydai@users.noreply.github.com>
2019-10-15 21:30:59 -0700

    Create LongestPalindrome.js

---
+++ LongestPalindrome.js
@@ -0,0 +1,35 @@
+var longestPalindrome = function(s) {
+    let palindromes = [];
+    for(let i = 0; i < s.length; i++) {
+        for(let j = i+1; j < s.length; j++) {
+            let substring = s.slice(i, j);
+            if(isPalindrome(substring)) {
+                palindromes.push(substring);
+            }
+        }
+    }
+    
+    let max = 0;
+    let longest = '';
+    for(let i = 0; i < palindromes.length; i++) {
+        if(palindromes[i].length > max) {
+            max = palindromes[i].length;
+            longest = palindromes[i];
+        }
+    }
+    return longest;
+};
+
+function isPalindrome(s) {
+    let start = 0;
+    let end = s.length - 1;
+    
+    while(start <= end) {
+        if(s[start] !== s[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
